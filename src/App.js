c1da300f62cca8c3555414f10a157346b29d2c2a
blackmozzzart <blackmozzzart@ya.ru>
2021-12-08 00:30:23 +0300

    Провели краткий курс по функциям

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ function App() {
   const [newToDo, setNewToDo] = React.useState('');
   const onHandleChange = (event) => {
     setNewToDo(event.target.value)};
-  console.log(newToDo);
+  // console.log(newToDo);
   const [toDoList, setToDoList] = React.useState(['Что нибудь?', 'Вспомнил?'])
-  const onHandleClick = (event) => {
+  const addToDo = (event) => {
     if (newToDo !== '') {
       setToDoList([newToDo, ...toDoList])
       setNewToDo('')
     }
   }
+  const deleteToDo = (event) => {
+    console.log('deleteToDo')
+  }
   return (
     <div className="App">
       <input type='text' value={newToDo} onChange={onHandleChange}/>
-      <button onClick={onHandleClick}>Подтвердить</button>
+      <button onClick={addToDo}>Подтвердить</button>
       <ul>{toDoList.map((toDo, index) => {
         const key = toDo + '_' + index;
-        console.log(key);
+        // console.log(key);
         return (
-          <ToDo ToDo={toDo}/>
+          <ToDo text={toDo}/>
         )
       })}</ul>
     </div>
--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,6 +1,12 @@
 import './ToDo.css'
 export function ToDo(props) {
+    console.log(props)
+    const deleteToDo = () => {}
     return (
-        <li className='ToDo'>{props.ToDo}</li>
+        <div className="wrapper">
+            <li className='ToDo'>{props.text}</li>
+            <button className='button' onClick={deleteToDo}>Удалить</button>
+            <button className='button'>Редактировать</button>
+        </div>
     )
 }
