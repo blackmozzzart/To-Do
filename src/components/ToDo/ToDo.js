b160af007fd1d46c5ec32ee72509c363d8e4f2a6
Атила Мурат Юсуфович <atila@vtb.ru>
2021-12-09 01:10:12 +0300

    редактирование элементов

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,20 +1,37 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 import './ToDo.css'
+
+
+
 export function ToDo(props) {
+    const [hasInput, setHasInput] = useState(false);
     // props - объект, с полями которые были переданы в роди
     console.log(props);
-    
+
     const deleteToDo = () => {
         props.deleteFunction(props.text);
-        // console.log('delete', props.text)
+        // console.log('delete', props.text);
+    }
+
+    const editTodo = () => {
+        console.log('<---START--->');
+
+        if (hasInput === true) {
+            setHasInput(false);
+        } else {
+            setHasInput(true);
+        }
+
+        console.log('<---END--->');
     }
 
     // TODO: сделать обработку пропсы цвета (color), чтобы цвет текста менялся если пропса передана
     return (
         <div className="wrapper">
-            <li className='ToDo'>{props.text}</li>
+            {hasInput ? <input type="text" defaultValue={props.text} /> : <li className='ToDo'>{props.text}</li>}
             <button className='button' onClick={deleteToDo}>Удалить</button>
-            <button className='button'>Редактировать</button>
+            <button className='button' onClick={editTodo}>{hasInput ? 'Сохранить' : 'Редактировать'}</button>
         </div>
     )
 }
